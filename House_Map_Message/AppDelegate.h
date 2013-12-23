253166b4692d763beb5db144bc7d3af45eaca315
loohcs <935552718@qq.com>
2013-12-23 09:57:39 +0800

    Initial Commit

---
+++ House_Map_Message/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  House_Map_Message
+//
+//  Created by 1007 on 13-12-23.
+//  Copyright (c) 2013年 Ibokan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ House_Map_Message/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  House_Map_Message
+//
+//  Created by 1007 on 13-12-23.
+//  Copyright (c) 2013年 Ibokan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
